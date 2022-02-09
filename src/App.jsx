cf2981aa33bef8d9bf3f601b5519edde00c524ff
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-09 13:43:10 +0100

    Refactor App module after change names of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Fragment } from 'react/cjs/react.production.min';
 import React from 'react';
-import SearchLaunche from './components/SearchLaunche';
+import FilterInput from './components/FilterInput';
 import ListOfLaunches from './components/ListOfLaunches';
 
 function App() {
   return (
     <Fragment>
       <h1>Search SpaceX Launches</h1>
-      <SearchLaunche />
+      <FilterInput />
       <ListOfLaunches />
     </Fragment>
 

47badb33f57cf7f118be5ebba74fea3d458b0daf
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-09 17:39:49 +0100

    Update ListOfLaunches
    Import LaunchCard component
    Add default parameters on getLaunches
    Add list on template
    Pass props to LaunchCard

--- src/components/ListOfLaunches.jsx
+++ src/components/ListOfLaunches.jsx
@@ -1,25 +1,49 @@
 /* eslint-disable no-console */
 import React, { useEffect, useState } from 'react';
 import getLaunches from '../helpers/getLaunches';
+import LaunchCard from './LaunchCard';
 
 function ListOfLaunches() {
   const [pastLaunches, setLaunches] = useState([]);
+  // const [key, setKet] = useState('');
+  // const [value, setValue] = useState('');
 
   useEffect(() => {
-    getLaunches()
-      .then(setLaunches);
+    getLaunches({ key: 'launch_year', value: '2018' })
+      .then((data) => setLaunches(data));
   }, []);
+
   console.log('past launches on list', pastLaunches);
 
   return (
     <>
-      <h1>Lis of Past Launches</h1>
+      <h1>List of Past Launches</h1>
       <p>
         Number of Launches:
         {' '}
         {pastLaunches.length}
       </p>
-      <p>{pastLaunches[0].flight_number}</p>
+      <ul>
+        {pastLaunches.map((launch) => (
+          <LaunchCard
+            key={launch.static_fire_date_unix}
+            missionName={launch.mission_name}
+            launchYear={launch.launch_year}
+            flightNumber={launch.flight_number}
+            customers={launch.rocket.second_stage.payloads
+              .map((payload) => (payload.customers
+                .map((customer) => (
+                  <li
+                    key={launch.flight_number}
+                  >
+                    {customer}
+
+                  </li>
+                ))
+              ))}
+          />
+        ))}
+      </ul>
     </>
   );
 }

30e8c12cdcefd362cdcfe3220523e0623928831b
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-09 17:35:57 +0100

    Refactos getLaunches module
    Add key and value as parameters of getLaunches function
    Delete comments

--- src/helpers/getLaunches.js
+++ src/helpers/getLaunches.js
@@ -1,27 +1,13 @@
 /* eslint-disable no-console */
-const getLaunches = () => {
-  const apiURL = 'https://api.spacexdata.com/v3/launches/past';
+const getLaunches = ({ key = 'launch_year', value = '2018' } = {}) => {
+  const apiURL = `https://api.spacexdata.com/v3/launches/?${key}=${value}`;
 
   return fetch(apiURL)
     .then((res) => res.json()
       .then((response) => {
         const data = response;
-        console.log('data en fetch', data);
         return data;
       }));
 };
-getLaunches();
 
 export default getLaunches;
-
-// import axios from 'axios';
-
-// const getLaunches = async () => {
-//   const apiURL = 'https://api.spacexdata.com/v3/launches/past';
-//   let launchesData = [];
-//   const { data } = await axios.get(apiURL);
-//   launchesData = data;
-//   console.log('launchesdata on get', typeof launchesData);
-//   return launchesData;
-// };
-// export default getLaunches;

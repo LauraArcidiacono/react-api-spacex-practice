50b20dbd8555f1497fee5a62bf02d41c0c80615e
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-09 13:44:50 +0100

    Refactor ListOfLaunches component
    Use useState and useEffect hooks

--- src/components/ListOfLaunches.jsx
+++ src/components/ListOfLaunches.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
 import getLaunches from '../helpers/getLaunches';
 
 function ListOfLaunches() {
-  const launches = getLaunches();
+  const [pastLaunches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    getLaunches()
+      .then(setLaunches);
+  }, []);
+  console.log('past launches on list', pastLaunches);
+
   return (
     <>
-      <h1>List Of SpaceX Launches</h1>
-      <div>{launches}</div>
+      <h1>Lis of Past Launches</h1>
+      <p>
+        Number of Launches:
+        {' '}
+        {pastLaunches.length}
+      </p>
+      <p>{pastLaunches[0].flight_number}</p>
     </>
   );
 }

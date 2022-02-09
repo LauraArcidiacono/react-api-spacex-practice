2990842bad41ea6d361ac552648cddf4334aa5e4
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-09 17:37:38 +0100

    Update LaunchCard module
    Add props and propTypes
    Add template

--- src/components/LaunchCard.jsx
+++ src/components/LaunchCard.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function LauncheCard() {
+function LauncheCard({
+  missionName,
+  launchYear,
+  flightNumber,
+  customers
+}) {
   return (
     <>
-      <h1>Card of Launch</h1>
-      <p>Launch 1</p>
+      <h1>
+        {' '}
+        {missionName}
+      </h1>
+      <p>
+        Launch Year:
+        {' '}
+        {launchYear}
+      </p>
+      <p>
+        Flight Number:
+        {' '}
+        {flightNumber}
+      </p>
+      <p>
+        Customers:
+        {' '}
+        {customers}
+      </p>
+
     </>
   );
 }
 export default LauncheCard;
+
+LauncheCard.propTypes = {
+  missionName: PropTypes.string.isRequired,
+  launchYear: PropTypes.string.isRequired,
+  flightNumber: PropTypes.number.isRequired,
+  customers: PropTypes.string.isRequired
+};

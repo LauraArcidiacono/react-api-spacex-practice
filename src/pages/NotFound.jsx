3776c6a8c5d28e5c44e017474b1fe4bbbe861fa6
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-09 17:36:31 +0100

    Create NotFound component

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <>
+      <h2> 404- Page Not Found</h2>
+      <p>Page Not Found</p>
+      <h3>Please comeback to a save page</h3>
+    </>
+  );
+}
+export default NotFound;

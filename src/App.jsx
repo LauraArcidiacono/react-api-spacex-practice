793b2240a2cc1e02a1509e1552d37e688436340a
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-09 18:06:15 +0100

    Create Header component
    Add Header component to App module

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Details from './pages/Details';
 import NotFound from './pages/NotFound';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
-      <h1>Search SpaceX Launches</h1>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/launcheDetails/:id" element={<Details />} />
+        <Route path="/launchDetails/:id" element={<Details />} />
         <Route path="*" exact element={<NotFound />} />
       </Routes>
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <h1>Search SpaceX Launches</h1>
+  );
+}
+export default Header;

6c857990a750de9a9d6e604c86bf89cb6b689c25
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-09 13:45:43 +0100

    Refactor getLaunches helper

--- src/helpers/getLaunches.js
+++ src/helpers/getLaunches.js
@@ -1,25 +1,27 @@
 /* eslint-disable no-console */
-// const getLaunches = async () => {
-//   const url = 'https://api.spacexdata.com/v3/launches/past';
-//   const resp = await fetch(url)
-//     .then((response) => response.json())
-//     .then((response) => {
-//       const { data } = response;
-//     });
-//   return resp;
-// };
+const getLaunches = () => {
+  const apiURL = 'https://api.spacexdata.com/v3/launches/past';
 
-// export default getLaunches;
-import axios from 'axios';
-
-const getLaunches = async () => {
-  const url = 'https://api.spacexdata.com/v3/launches/past';
-  try {
-    const { data } = await axios.get(url);
-    const apiData = await data;
-    return apiData;
-  } catch (err) {
-    throw Error(err.message);
-  }
+  return fetch(apiURL)
+    .then((res) => res.json()
+      .then((response) => {
+        const data = response;
+        console.log('data en fetch', data);
+        return data;
+      }));
 };
+getLaunches();
+
 export default getLaunches;
+
+// import axios from 'axios';
+
+// const getLaunches = async () => {
+//   const apiURL = 'https://api.spacexdata.com/v3/launches/past';
+//   let launchesData = [];
+//   const { data } = await axios.get(apiURL);
+//   launchesData = data;
+//   console.log('launchesdata on get', typeof launchesData);
+//   return launchesData;
+// };
+// export default getLaunches;

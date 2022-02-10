d5c55a4b518a12494fd2a80f10fbf5d048d62489
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-10 18:19:14 +0100

    Refactor ListOfLaunches module with useFilter custom hook

--- src/components/ListOfLaunches.jsx
+++ src/components/ListOfLaunches.jsx
@@ -1,24 +1,15 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable no-console */
 import propTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import getLaunches from '../helpers/getLaunches';
+import useFilter from '../hook/useFilter';
 import LaunchCard from './LaunchCard';
 
 function ListOfLaunches({ filterKey, filterValue }) {
-  const [pastLaunches, setLaunches] = useState([]);
+  const { pastLaunches, loading } = useFilter(filterKey, filterValue);
 
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    getLaunches(filterKey, filterValue)
-      .then((data) => {
-        setLaunches(data);
-        setLoading(false);
-      });
-  }, [filterKey, filterValue]);
+  console.log(pastLaunches);
 
   if (loading) return <h2>Loading Launches...</h2>;
 

29b451489ace337d824df46a00430d94841850e8
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-10 16:16:03 +0100

    Add logic on Home
    Add state variables
    Add handleSubmit and handleChange functions
    Add props to FilterInput and ListOfLaunches components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
 import { Fragment } from 'react/cjs/react.development';
 import FilterInput from '../components/FilterInput';
 import ListOfLaunches from '../components/ListOfLaunches';
 
 function Home() {
+  const [key, setKey] = useState();
+  const [value, setValue] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    const keyValue = event.target.value;
+    const arrayKeyValue = keyValue.split(':');
+    const [searchedKey, searchedValue] = arrayKeyValue;
+
+    setKey(searchedKey);
+    setValue(searchedValue);
+  };
   return (
     <Fragment>
-      <FilterInput />
-      <ListOfLaunches />
+      <FilterInput
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+      />
+      <ListOfLaunches
+        filterKey={key}
+        filterValue={value}
+      />
     </Fragment>
   );
 }

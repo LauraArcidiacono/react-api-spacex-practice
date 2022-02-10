6784cc8e2a4f80f467a5cefa55480f3038a932e9
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-10 16:07:04 +0100

    Set getLaunches to return an object with selected properties

--- src/helpers/getLaunches.js
+++ src/helpers/getLaunches.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-console */
-const getLaunches = ({ key = 'launch_year', value = '2018' } = {}) => {
+const getLaunches = (key = '', value = '') => {
   const apiURL = `https://api.spacexdata.com/v3/launches/?${key}=${value}`;
-
+  console.log('key y  value en getLaunches', key, value);
   return fetch(apiURL)
     .then((res) => res.json()
       .then((response) => {
         const data = response;
-        return data;
+        const launchesArray = data.map((launch) => ({
+          flightNumber: launch.flight_number,
+          missionName: launch.mission_name,
+          launchYear: launch.launch_year,
+          payloadsCount: launch.rocket.second_stage.payloads.length,
+          customers: launch.rocket.second_stage.payloads.map(
+            (customer) => customer.customers
+          ).toString()
+        }));
+        return launchesArray;
       }));
 };
 

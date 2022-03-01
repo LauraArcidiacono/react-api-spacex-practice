91fcf64e778166dbe6d67efbee9e06937b522868
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-03-01 15:13:54 +0100

    Refactor useFilter hook
    Add filteredLaunches constant
    Set useEffect with filterKey and filterValue
    Return filteresLaunches

--- src/hook/useFilter.js
+++ src/hook/useFilter.js
@@ -1,35 +1,36 @@
+/* eslint-disable no-console */
 import { useState, useEffect } from 'react';
 import getLaunches from '../helpers/getLaunches';
 
 const useFilter = (filterKey, filterValue) => {
   const [pastLaunches, setLaunches] = useState([]);
-
+  const [filteredLaunches, setFilteredLaunches] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    getLaunches(filterKey, filterValue)
+    getLaunches()
       .then((data) => {
-        console.log(data);
-        if (filterKey === 'customers') {
-          const customerFilter = data.filter(
-            (launch) => launch.customers.toLowerCase().indexOf(filterValue.toLowerCase()) !== -1
-          );
-          setLaunches(customerFilter);
-        } else if (filterKey === 'mission_name') {
-          const missionNameFilter = data.filter(
-            (launch) => launch.missionName.toLowerCase().indexOf(filterValue.toLowerCase()) !== -1
-          );
-          setLaunches(missionNameFilter);
-        } else {
-          setLaunches(data);
-        }
+        setLaunches(data);
+        setFilteredLaunches(data);
         setLoading(false);
       });
+  }, []);
+
+  useEffect(() => {
+    if (filterKey === 'customers' || filterKey === 'missionName') {
+      setFilteredLaunches(pastLaunches.filter(
+        (launch) => launch[filterKey].toLowerCase().indexOf(filterValue.toLowerCase()) !== -1
+      ));
+    } else if (filterKey === 'flightNumber' || filterKey === 'launchYear') {
+      setFilteredLaunches(pastLaunches.filter(
+        (launch) => launch[filterKey] === filterValue
+      ));
+    }
   }, [filterValue]);
 
   return {
-    pastLaunches,
+    filteredLaunches,
     loading
   };
 };

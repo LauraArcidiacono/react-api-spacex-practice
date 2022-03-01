e53e158589b4ce514cb27182dbb3ae3df73d663c
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-03-01 15:10:37 +0100

    Update getLaunches
    Delete parameters
    Transform flightNumbre to string

--- src/helpers/getLaunches.js
+++ src/helpers/getLaunches.js
@@ -1,14 +1,12 @@
-const getLaunches = (key = '', value = '') => {
-  let apiURL = 'https://api.spacexdata.com/v3/launches/';
-
-  apiURL += (key !== '' && value !== '') ? `?${key}=${value}` : '';
+const getLaunches = () => {
+  const apiURL = 'https://api.spacexdata.com/v3/launches/';
 
   return fetch(apiURL)
     .then((res) => res.json()
       .then((response) => {
         const data = response;
         const launchesArray = data.map((launch) => ({
-          flightNumber: launch.flight_number,
+          flightNumber: launch.flight_number.toString(),
           missionName: launch.mission_name,
           launchYear: launch.launch_year,
           payloadsCount: launch.rocket.second_stage.payloads.length,
